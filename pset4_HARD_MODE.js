4a914b4e8da481addb59806746bbbfc110e5f619
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-09-30 16:02:45 -0400

    Create pset4_HARD_MODE.js

---
+++ pset4_HARD_MODE.js
@@ -0,0 +1,249 @@
+/*
+    In this PSET, we will attempt to recreate select Array methods
+    The first few will be simpler, the last few will not
+    
+    Each problem will point back to the MDN documentation associated
+    with the method. Consult the docs for information on how to
+    implement (mainly a time saving measure, writing jsdocs is time
+    consuming)
+    
+    Some problems may have additional challenges to consider.
+*/
+
+/*  1
+    @function Array.pop()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+
+    Intended to be: easy
+    CHALLENGE: do not use slice/splice/etc, just loops
+*/
+
+/*  2
+    @function Array.push()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+    
+    Intended to be: easy
+    CHALLENGE: do not use slice/splice/etc, just loops
+*/
+
+/*  3
+    @function Array.shift()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+
+    Intended to be: easy
+    CHALLENGE: do not use slice/splice/etc, just loops
+*/
+
+/*  4
+    @function Array.unshift()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+
+    Intended to be: easy
+    CHALLENGE: do not use slice/splice/etc, just loops
+*/
+
+/*  5
+    @function Array.join()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+    Intended to be: easy
+*/
+
+/*  6
+    @function Array.indexOf()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+
+    Intended to be: easy
+*/
+
+
+/*  7
+    @function Array.lastIndexOf()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf
+
+    Intended to be: easy
+*/
+
+/*  8
+    @function Array.includes()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+
+    Intended to be: easy
+*/
+
+/*  9
+    @function Array.concat()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+    Intended to be: easy
+*/
+
+
+/*  10
+    @function Array.entries()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/entries
+
+    Intended to be: easy
+
+    NOTE: NO NEED TO RETURN ITERATOR
+    Just return an array of arrays containing entries
+    For example:
+        [1,2,3,4].entries();
+        // [[0,1], [1,2], [2,3], [3,4]]
+*/
+
+
+/*  11
+    @function Array.every()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+
+    Intended to be: hard
+
+    CHALLENGE:
+        1. implement with just for loops
+        2. implement again, but with reduce function
+        3. implement again, but with filter
+*/
+
+/*  12
+    @function Array.fill()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
+
+    Intended to be: hard
+
+    CHALLENGE:
+        1. implement with just for loops
+        2. implement again, but with reduce function
+        3. implement again, but with map
+*/
+
+/* 13
+    @function Array.some()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+
+    Intended to be: hard
+
+    CHALLENGE:
+        1. implement with just for loops
+        2. implement again, but with reduce function
+        3. consider optimizations that will exit as soon as match criteria is found
+
+*/
+
+/* 14
+    @function Array.find()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+
+    Intended to be: hard
+
+    CHALLENGE:
+        1. implement with just for loops
+        2. implement again, but with reduce function
+        3. consider optimizations that will exit as soon as match criteria is found
+
+*/
+
+/* 15
+    @function Array.findIndex()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+
+    Intended to be: hard
+
+    CHALLENGE:
+        1. implement with just for loops
+        2. implement again, but with reduce function
+        3. consider optimizations that will exit as soon as match criteria is found
+
+*/
+
+
+/* 16
+    @function Array.reduceRight()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduceRight
+
+    Intended to be: hard
+
+    CHALLENGE:
+        1. implement with just for loops
+        2. implement again, but with reduce function
+
+*/
+
+/* 17
+    @function Array.reverse()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+
+    Intended to be: hard
+
+    CHALLENGE:
+        1. implement with just for loops
+        2. implement again, but with reduce function
+        3. implement again, using recursion
+
+*/
+
+/* 18
+    @function Array.slice()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+
+    Intended to be: hard
+
+    CHALLENGE:
+        1. implement with just for loops
+        2. implement again, but with reduce function
+
+*/
+
+/*  19
+    @function Array.flatten()
+    @description:
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatten
+
+    Intended to be: hard
+
+    NOTE:
+        1. It is simplest to implement recursively (google "beginner's intro to recursion" for some context)
+
+    CHALLENGE:
+        1. consider implementing this recursively for N levels
+        2. consider implementing this with Array.reduce() (will still need recursion)
+*/
+
+
