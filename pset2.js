2b3e6eca2486d90e6f5fc05c9371c7e01ad6b373
vincentgalarza <vincentgalarza@gmail.com>
2018-10-20 18:03:35 -0400

    completed PSET2

--- pset2.js
+++ pset2.js
@@ -6,6 +6,19 @@
     @example - removeNegatives([-3,-2,1,2,3,-4]); // [3,2,1,2,3,4]
 */
 
+const removeNegatives = (arr) => {
+    const newArr = [];
+    for (i = 0; i < arr.length; i++) {
+        if (!(arr[i] >= 0)) {
+            newArr.push(arr[i] * -1);
+        } else {;
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+console.log(removeNegatives([ -3, -2, 1, 2, 3, -4 ]), `[ 3, 2, 1, 2, 3, 4 ]`);
+
 /*
     @func findVal
     @param {array} arr
@@ -20,6 +33,24 @@
         findVal([1,2,3,4], 9); // -1
 */
 
+const findVal = (arr, val) => {
+    const storeArray = [];
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            storeArray.push(i);
+            console.log(`storyArray now has... ${storeArray}`)
+        }
+    }
+
+    if (storeArray.length > 0) {
+        return (`Value found at indices: ${storeArray}`);
+    } else {
+        return -1;
+    }
+}
+
+console.log(findVal([ 1, 2, 3, 4, 1 ], 1));
+
 /*
     @func removeOdds
     @param {array} arr
@@ -32,6 +63,20 @@
         removeOdds([1,"2",3,4]); // [4]
 */
 
+const removeOdds = (arr) => {
+    newArr = [...arr];
+    for (i = 0; i < newArr.length; i++) {
+        console.log(typeof newArr[i])
+        if (typeof newArr[i] !== 'number' || newArr[i] % 2 !== 0){
+            newArr.splice(i, 1);
+        }
+    }
+    return newArr;
+}
+
+console.log(removeOdds([ 1, 2, 3, 4]), '[ 2, 4 ]');
+console.log(removeOdds([ 1, "2", 3, 4]), '[ 4 ]');
+
 /*
     @func addSquares
     @param {array} arr
@@ -41,6 +86,16 @@
         addSquares([1,2,3,4); // [1,2,3,4,1,4,9,16]
 */
 
+const addSquares = (arr) => {
+    const newArr = [...arr];
+    for (i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] ** 2);
+    }
+    return newArr;
+}
+
+console.log(addSquares([ 1, 2, 3, 4]), '[ 1, 2, 3, 4, 1, 4, 9, 16 ]')
+
 /*
     @func doubleify
     @param {array} arr
@@ -49,6 +104,15 @@
     @example - doubleify([1,2,3,4); // [1,1,2,2,3,3,4,4]
 */
 
+const doubleify = (arr) => {
+    const newArr = [];
+    for (i = 0; i < arr.length; i++) {
+        newArr.push(arr[i], arr[i])
+    }
+    return newArr;
+}
+
+console.log(doubleify([ 1, 2, 3, 4]), '[ 1, 1, 2, 2, 3, 3, 4, 4]')
 
 /*
     @func findMax
@@ -57,3 +121,15 @@
     @desc - find largest number in array
     @example - findMax([1,2,3,99,4]); // 99 
 */
+
+const findMax = (arr) => {
+    let currentMax = arr[0];
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] >= currentMax) {
+            currentMax = arr[i]
+        }
+    }
+    return currentMax
+}
+
+console.log(findMax([ 1, 2, 3, 99, 4]), '99');
